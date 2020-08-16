fce4e20a57c743c7427f33e0fb6cbdb538a0625c
Hamed Bensaad <hamed.abdesselam7@yahoo.com>
2020-08-16 14:00:18 +0100

    feat: toggle Navbar menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,36 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../static/images/logo.svg";
+import menu from "../../static/images/menu.svg";
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className={"container"}>
-      <div className={"top-menu"}>
-        <div className={"logo"}>
-          <Link to="/" title={siteTitle}>
+const Header = ({ siteTitle }) => {
+  const [responsive, setResponsive] = useState(false);
+
+  return (
+    <header>
+      <div className={"container"}>
+        <div className={`topnav ${responsive ? "responsive" : ""}`}>
+          <Link to="/" title={siteTitle} className="logo">
             <img alt={"Logo"} src={logo} />
           </Link>
-        </div>
-
-        <div className={"get-started"}>
-          <a href="#">Télécharger</a>
+          <Link to="/docs" href="#">
+            Documentation
+          </Link>
+          <Link to="/contact" href="#">
+            Contact
+          </Link>
+          <Link to="/about" href="#">
+            About
+          </Link>
+          <a className="icon" onClick={() => setResponsive(!responsive)}>
+            <img alt="Toggle" src={menu} width="40px" />
+          </a>
         </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string
