3e4966fcfa518e8a986d86d39ca137d63eee448b
Hamed Bensaad <hamed.abdesselam7@yahoo.com>
2020-08-17 11:13:26 +0100

    fix: pathPrefix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ module.exports = {
     short: `evo`,
     description: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.`,
     author: `@vhamed`,
-    siteUrl: `https://vhamed.github.io/evo`,
-    pathPrefix: "/evo"
+    siteUrl: `https://vhamed.github.io/evo`
   },
+  pathPrefix: "/evo",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
